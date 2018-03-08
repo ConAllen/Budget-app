6e21b393404cae68cc7451a24adf610d78843d82
Your Name <you@example.com>
2018-03-08 12:22:20 +0000

    budget controller - function constructors

--- app.js
+++ app.js
@@ -3,9 +3,35 @@
 
 var budgetController = (function(){
 
-    // some code
+    // expense function consctructor methods will be added by prototype
+    var Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    // income function consctuctor  methods will be added by prototype
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+
+        allItems: {
+            Expenses: [],
+            Incomes: []
+        },
+
+        totals: {
+            Expenses:0,
+            Incomes:0
+        }
+    };
 
 })();
+
+
 
 // UI Controller
 var UIController = (function(){
@@ -14,10 +40,10 @@ var UIController = (function(){
 
         inputType:'.add__type',
         inputDescription:'.add__description',
-        inputValue:'.add__value'
+        inputValue:'.add__value',
+        inputBtn: '.add__btn'
 
     };
-
     return {
         getInput: function() {
             return {
@@ -25,12 +51,13 @@ var UIController = (function(){
               type: document.querySelector(DOMStrings.inputType).value, // will be either inc or exp
               description: document.querySelector(DOMStrings.inputDescription).value,
               value: document.querySelector(DOMStrings.inputValue).value
-
             }
+        },
+        // this method has now made domstring a public method so it can be passed into the other controllers
+        getDOMStrings: function(){
+            return DOMStrings;
         }
     }
-
-
 })();
 
 
@@ -38,6 +65,20 @@ var UIController = (function(){
 
 
 var controller = (function(budgetCtrl,UICtrl){
+
+    var setUpEventListeners = function(){
+
+         // this method is carried in from the UICtrl to eliminate the DOM strings
+        var DOM = UICtrl.getDOMStrings();
+        // event listeners
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', function(event){
+            if (event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+        });
+
+    }
 
     var ctrlAddItem = function(){
     // 1. get the filed input data
@@ -51,19 +92,17 @@ var controller = (function(budgetCtrl,UICtrl){
     // 4. calculate the budget
 
     // 5. display the budget
+    };
 
-    }
+    return {
+        init: function(){
+            setUpEventListeners();
+        }
 
-document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    };
 
-document.addEventListener('keypress', function(event){
 
-    if (event.keyCode === 13 || event.which === 13){
-
-        ctrlAddItem();
-
-    }
-
-});
 
 })(budgetController, UIController);
+
+controller.init();
